da897e1a61c8edb4720967bb07afc1be11fee0ee
Your Name <you@example.com>
2025-11-04 10:42:18 +0200

    asdasd

--- cyberhunt-ar/vite.config.js
+++ cyberhunt-ar/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cyberhunt-ar/', 
+  base: '/arapp/cyberhunt-ar/', 
   plugins: [
     react({
       babel: {
